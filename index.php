104b07dc89962a2223eaf52c02aa1a21d598825d
Murad Rustamov <mrustamov@studenten.hs-bremerhaven.de>
2020-06-12 00:53:49 +0200

    initial

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Wallace</title>
+</head>
+
+<body>
+    <main class="wrap-it">
+        Hello, World!
+    </main>
+</body>
+
+</html>
---
+++ watch.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+sass --watch --no-cache assets/sass/style.sass:assets/css/style.min.css --style compressed
